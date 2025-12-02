38c41710dde91eb67753e77ed8bf20f381c888c1
Victor321-coder <440004873@gapps.yrdsb.ca>
2025-12-02 14:13:14 -0500

    something

--- Apple.java
+++ Apple.java
@@ -12,6 +12,8 @@ public class Apple extends Actor
      * Act - do whatever the Apple wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    int speed = 1; 
+    
     public void act()
     {
         setLocation(getX(), getY() + 3);
@@ -21,5 +23,9 @@ public class Apple extends Actor
             world.gameOver();
             world.removeObject(this);
         }
+    }
+    
+    public void setSpeed(int spd) {
+        
     }
 }
